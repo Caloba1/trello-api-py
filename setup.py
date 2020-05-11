03fede65b8bf4cd1b13ee685a1e5e21bcf2fbf57
Michael Daniel Caloba <michaelcaloba@gmail.com>
2020-05-11 15:43:50 +0200

    Edited Github Link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Updated and extended version of py-trello",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Caloba1/trello-api-py",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
